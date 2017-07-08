c212afb8f983ea60d1da88fe7dbc860b121f74de
Casper van Wezel <casper@poki.com>
2017-07-08 21:23:13 +0200

    Now also copies the files

--- index.go
+++ index.go
@@ -12,27 +12,37 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
-const diskIndexDB = "./file-index.db"
+const (
+	diskIndexDB = "./file-index.db"
+	breakPoint  = 10000
+)
 
 var db *sql.DB
 
 func main() {
+	//init
+	os.Remove(diskIndexDB)
+	os.Create("indexer.txt")
+	os.Create("copy.txt")
+
 	bootDatabase()
 
 	diskA := os.Args[1]
 	diskB := os.Args[2]
+	dest := os.Args[3]
 
 	getFilesFromFolder(true, diskA)
 	getFilesFromFolder(false, diskB)
 
 	fmt.Println("--- FINISHED INDEXING ---")
+
+	files := findMissingFiles()
+	copyFiles(dest, files)
+
 	db.Close()
 }
 
 func bootDatabase() {
-	//Delete previous DB
-	os.Remove(diskIndexDB)
-
 	var err error
 
 	//We're going to store disk A information in a SQLite database
@@ -58,20 +68,34 @@ func insertFile(origin bool, filePath string) {
 	//Not really sure what to do if there is an error, so let's log and quit to be safe. We don't want to mis files
 	stmt, err := db.Prepare("INSERT INTO file(origin, hash, path) VALUES (?,?,?)")
 	if err != nil {
-		log.Fatal("DB-PREPARE: Failed on file: "+filePath, err)
+		redoLog("indexer", filePath)
+		return
 	}
 
-	_, err = stmt.Exec(origin, getMD5FromFile(filePath), filePath)
+	md5, err := getMD5FromFile(filePath)
 	if err != nil {
-		log.Fatal("DB-INSERT: Failed on file: "+filePath, err)
+		redoLog("indexer", filePath)
+		return
+	}
+
+	_, err = stmt.Exec(origin, md5, filePath)
+	if err != nil {
+		redoLog("indexer", filePath)
+		return
 	}
 }
 
 func getFilesFromFolder(origin bool, searchDir string) {
 	fmt.Println("Indexing folder: " + searchDir)
 
+	var count int64 = 0
+
 	filepath.Walk(searchDir, func(path string, f os.FileInfo, err error) error {
 		if !f.IsDir() {
+			count++
+			if count%breakPoint == 0 {
+				fmt.Println(count)
+			}
 			insertFile(origin, path)
 		}
 
@@ -79,17 +103,111 @@ func getFilesFromFolder(origin bool, searchDir string) {
 	})
 }
 
-func getMD5FromFile(filePath string) string {
+func getMD5FromFile(filePath string) (string, error) {
 	f, err := os.Open(filePath)
 	if err != nil {
-		log.Fatal("MD5 OPEN: ", filePath, err)
+		return "", err
 	}
 	defer f.Close()
 
 	h := md5.New()
 	if _, err := io.Copy(h, f); err != nil {
-		log.Fatal("MD5 IO COPY: ", filePath, err)
+		return "", err
 	}
 
-	return fmt.Sprintf("%x", h.Sum(nil))
+	return fmt.Sprintf("%x", h.Sum(nil)), nil
+}
+
+func findMissingFiles() []string {
+	//We left join to get the missing files and retrieve only the ones from the source
+	missingQuery := `
+	SELECT source.path,
+		destination.id AS missing
+	FROM   FILE source
+		LEFT JOIN FILE destination
+				ON source.hash = destination.hash
+					AND source.origin = 1
+					AND destination.origin = 0
+	WHERE  missing IS NULL
+		AND source.origin = 1`
+
+	rows, err := db.Query(missingQuery)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	files := []string{}
+	for rows.Next() {
+		var path string
+		var weDoNotCare interface{}
+		err = rows.Scan(&path, &weDoNotCare)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		files = append(files, path)
+	}
+
+	err = rows.Err()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return files
+
+}
+
+func copyFiles(dir string, files []string) {
+	err := os.MkdirAll(dir, os.ModePerm)
+	if err != nil {
+		log.Fatal("COPY INIT: Could not create copy-directory")
+	}
+
+	for _, file := range files {
+		err = copyFile(file, dir+file)
+
+		if err != nil {
+			redoLog("copy", file)
+		}
+	}
+}
+
+func copyFile(src string, dst string) (err error) {
+	//Create output dir
+	dir := filepath.Dir(dst)
+	os.MkdirAll(dir, os.ModePerm)
+
+	//Do the copy
+	in, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer in.Close()
+
+	out, err := os.Create(dst)
+	if err != nil {
+		return err
+	}
+
+	defer func() {
+		cerr := out.Close()
+		if err == nil {
+			err = cerr
+		}
+	}()
+
+	if _, err = io.Copy(out, in); err != nil {
+		return err
+	}
+
+	return out.Sync()
+}
+
+//Dirty function to write the filepaths that crashedd to a file for after-processing
+func redoLog(tag string, path string) {
+	f, _ := os.OpenFile(tag+".txt", os.O_APPEND|os.O_WRONLY, os.ModeAppend)
+	f.WriteString(path + "\n")
+	f.Close()
 }
