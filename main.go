d549ee1c0998faa0f393ed789a8bb51fc1e094c8
Casper van Wezel <casper@poki.com>
2017-07-08 22:34:05 +0200

    Added the win :\\ path hack again

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"strings"
 
 	_ "github.com/mattn/go-sqlite3"
 )
@@ -179,7 +180,7 @@ func copyFiles(dir string, files []string) {
 			fmt.Println(fmt.Sprintf("%d%%", percentage))
 		}
 
-		err = copyFile(file, dir+file) //Windows does not like ex. C:\ in the path as new folder, so hack it away
+		err = copyFile(file, dir+strings.Replace(file, ":\\", "", -1)) //Windows does not like ex. C:\ in the path as new folder, so hack it away
 
 		if err != nil {
 			redoLog("copy", file)
